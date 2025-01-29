c616c19618c63178236adc0a955f5b75e803c45e
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-29 18:18:26 +0600

    Add files via upload

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,25 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Component
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Fahim",
+                        "fahim.nur@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        24
+                )
+        );
+
+
+    }
+}
+
